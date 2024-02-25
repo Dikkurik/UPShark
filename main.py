fbfd5969a7d1cb53744eada69d22982df1183916
Dikkurik <oimatetb@gmail.com>
2024-02-25 22:42:43 +0300

    added entel

--- main.py
+++ main.py
@@ -3,49 +3,89 @@
 import requests, json
 from bs4 import BeautifulSoup
 
-print('Started UPShark script')
+print('Started UPShark script ver 1.02\n')
 
-def getEatonPage():
-    with open('ups_list.json', 'r+', encoding='utf-8') as file:
-            ups_list = json.load(file)
-    
-    for i in (ups_list['eaton']):
-        #Собираем URL 
-        combUrl = f'http://{ups_list['eaton'][i]}/ups_propAlarms.htm'
-        print(combUrl)
-        try:
-            req = requests.get(ups_list['eaton'][i])
-            print(i, '*****')
-            print('!INFO Successful connected to UPS at destination', ups_list['eaton'][i])
-            req.encoding = "utf-8"
-            page = req.text
-            checkError_Eaton(page)
-            print('\n')
+class GetUPS():
+    def __init__(self):
+        with open('ups_list.json', 'r+', encoding='utf-8') as file:
+                self.ups_list = json.load(file)
+
+    def getEatonPage(self):
+        
+        for i in (self.ups_list['eaton']):
+            #Собираем URL 
+            combUrl = 'http://'+self.ups_list['eaton'][i]+'/ups_propAlarms.htm'
+            try:
+                req = requests.get(combUrl)
+                print(i, '***** EATON')
+                print('!INFO Successful connected to UPS at destination', self.ups_list['eaton'][i])
+                req.encoding = "utf-8"
+                page = req.text
+                self.checkError_Eaton(page)
+                print('\n')
+            except:
+                print(i, '!ERROR Connection timed out')
+                print('\n')
+        input('press enter to exit')
+
+    def checkError_Eaton(self, page: str) -> int:
+        #Объявляем объект супа с данными страницы
+        soup = BeautifulSoup(page, 'lxml')
+        errors = []
+        errors = soup.find_all(class_='listline1')
+        try:        
+            if len(errors) > 0:
+                print(f'Обнаружено {len(errors)} ошибок!')
+                print('!!! UPS Alarm: CHECK UPS !!!')
+                return len(errors)
+            else:
+                print('Status OK')
+                return len(errors)
         except:
-            print(i, '!ERROR Connection timed out')
-            print('\n')
-    input('press enter to exit')
+            print('Something went wrong! Unexpected result')
 
-def checkError_Eaton(page: str) -> int:
-    #Объявляем объект супа с данными страницы
-    soup = BeautifulSoup(page, 'lxml')
-    errors = []
-    errors = soup.find_all(class_='listline1')
-    try:        
-        if len(errors) > 0:
-            print(f'Обнаружено {len(errors)} ошибок!')
-            print('!!! UPS Alarm: CHECK UPS !!!')
-            return len(errors)
-        else:
-            print('Status OK')
-            return len(errors)
-    except:
-        print('Something went wrong! Unexpected result')
+    def getEntelPage(self):
+        for i in self.ups_list['entel']:
+            combUrl = 'http://'+self.ups_list['entel'][i]['ipaddres']+'/Status.htm'
+            try:
+                req = requests.get(combUrl, 
+                                    auth=(
+                                    self.ups_list['entel'][i]['login'],
+                                    self.ups_list['entel'][i]['password']
+                                    ))
+                print(i, '***** ENTEL')
+                print('!INFO Successful connected to UPS at destination', self.ups_list['entel'][i]['ipaddres'])
+                page = req.text
+                self.checkError_Entel(page)
+
+                
+            except:
+                print('Connection timed out')
+                print(self.ups_list['entel'][i]['ipaddres'],
+                      self.ups_list['entel'][i]['login'],
+                      self.ups_list['entel'][i]['password'])
+                
+    def checkError_Entel(self, page):
+        soup = BeautifulSoup(page, 'lxml')
+        statuses = []
+        statuses = soup.find_all(class_='Tabtext')
+        statusOK = 'UPS Normal'
+        stat = list(statuses)
+        try:
+            if statusOK in str(stat):
+                print('Status OK')
+            else:
+                print('!!! UPS Alarm: CHECK UPS !!!')
+        except:
+            print('Something went wrong! Unexpected result')
+
 
 
 
 
 if __name__ == '__main__':
-   getEatonPage()
+    shark = GetUPS()
+    shark.getEntelPage()
+    input('press enter to exit')
 
 
